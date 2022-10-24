c7293e67c31ef66b47e730c33aab55f201feef6a
Alireza Tabrizi <alias1371@gmail.com>
2022-10-24 21:01:33 +0330

    search component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Search from "./components/Search";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="App w-full py-4 px-5">
+      <Search />
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+
+const Search = () => {
+    return (
+        <div className="w-full relative mb-3.5">
+            <input className="w-full p-4 pr-14 pl-4 rounded-lg shadow-[0px_0px_20px_rgba(0,0,0,0.15)]" placeholder="جستجوی نام فرد"/>
+            <span className="absolute right-6 top-1/2 translate-y-[-50%]">
+                <img src="/svg/Vector.svg" alt=""/>
+            </span>
+        </div>
+    )
+}
+
+export default Search
